0d9064b344446d177395900f6e43eb2bfc4ecb3e
Sean Murphy <seanchrismurphy@gmail.com>
2014-04-18 18:05:12 +1000

    add data merge

---
+++ Data_merge.R
@@ -0,0 +1,13 @@
+# A function I wrote to merge together two data frames where one or both have variables the other doesn't
+Datamerge <- function(data1, data2) {
+  add1 <- setdiff(colnames(data2), colnames(data1))
+  add2 <- setdiff(colnames(data1), colnames(data2))
+  hold1 <- data.frame(matrix(data = NA, nrow = nrow(data1), ncol=length(add1)))
+  hold2 <- data.frame(matrix(data = NA, nrow = nrow(data2), ncol=length(add2)))
+  colnames(hold1) <- add1
+  colnames(hold2) <- add2
+  end1 <- cbind(data1, hold1)
+  end2 <- cbind(data2, hold2)
+  final <- rbind(end1, end2)
+  final
+}
